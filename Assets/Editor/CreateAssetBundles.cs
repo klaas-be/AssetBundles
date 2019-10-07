d31659f57c4b0bd2821b4ad4caf9b0ea724367fc
Klaas Behrendt <mail@klaas-behrendt.de>
2019-10-07 16:52:15 +0200

    initial commit

---
+++ Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Windows;
+/// <summary>
+/// Create a new shortcut for the assset dropdown: Build AssetBundles
+/// </summary>
+public class CreateAssetBundles
+{
+   [MenuItem("Assets/Build AssetBundles")]
+   static void BuildAllAssetBundles()
+   {
+      string assetBundleDirectory = "Assets/StreamingAssets";
+
+      if (!Directory.Exists(Application.streamingAssetsPath))
+      {
+         Directory.CreateDirectory(assetBundleDirectory);
+      }
+
+      BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None,
+         EditorUserBuildSettings.activeBuildTarget);
+   }
+}
